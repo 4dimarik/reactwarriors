11142c32c43429a9abd3182eb5149e3ee1ac0a31
4dimarik <4dimarik@gmail.com>
2020-04-20 12:59:21 +0300

    sdfdsf
    2dasdasd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import muviesData from "./muviesData";
+
+const movie = muviesData[0];
+
+console.log(movie);
+
+function Image(props) {
+  console.log(props);
+  return (
+    <p>
+      <img src={props.src} alt={props.alt} />
+    </p>
+  );
+}
+
+function MovieItem(props) {
+  const {
+    data: { title, vote_average, poster_path }
+  } = props;
+  return (
+    <div>
+      <Image src={poster_path} alt={title} />
+      <p>{title}</p>
+      <p>{vote_average}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <MovieItem data={movie} />
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
