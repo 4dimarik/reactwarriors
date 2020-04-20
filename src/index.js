ff1e015c9c85e7b8aaf46182963197741fab995a
4dimarik <4dimarik@gmail.com>
2020-04-20 16:05:26 +0300

    dsd

--- src/index.js
+++ src/index.js
@@ -5,21 +5,16 @@ import muviesData from "./muviesData";
 
 const movie = muviesData[0];
 
-console.log(movie);
-
 function Image(props) {
-  console.log(props);
   return (
     <p>
       <img src={props.src} alt={props.alt} />
     </p>
   );
 }
-
+/*
 function MovieItem(props) {
-  const {
-    data: { title, vote_average, poster_path }
-  } = props;
+  const { data: { title, vote_average, poster_path }} = props;
   return (
     <div>
       <Image src={poster_path} alt={title} />
@@ -27,6 +22,41 @@ function MovieItem(props) {
       <p>{vote_average}</p>
     </div>
   );
+}
+//
+*/
+
+class MovieItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false
+    };
+  }
+  render() {
+    const {
+      data: { title, vote_average, poster_path, overview }
+    } = this.props;
+    return (
+      <div>
+        <Image src={poster_path} alt={title} />
+        <p>{title}</p>
+        <p>{vote_average}</p>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({
+              show: true
+            });
+          }}
+        >
+          show
+        </button>
+        {this.state.show ? <p>{overview}</p> : null}
+      </div>
+    );
+  }
 }
 
 function App() {
